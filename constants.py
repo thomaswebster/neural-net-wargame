7e07720204bcacc9e1ccbc3cbec4e1c54f1e0387
Fela Maslen <felamaslen@gmail.com>
2016-11-07 19:07:10 +0000

    Spaced things properly in constants

--- constants.py
+++ constants.py
@@ -2,11 +2,15 @@
 
 
 #general constants for the project
-cnsts = {"SCREENSIZE":[320,320],
-    "TILE":16,
-    "MAPSIZE":[20,20]}
+param = {
+    "SCREENSIZE":   [320,320],
+    "TILE":         16,
+    "MAPSIZE":      [20,20]
+}
 
 #colouration of maptiles when drawn
-tilecolours = {0:(0,0,0), 
-    1:(255,255,255), 
-    2:(255,0,0)}
+tilecolours = {
+    0: (0,0,0),
+    1: (255,255,255),
+    2: (255,0,0)
+}
